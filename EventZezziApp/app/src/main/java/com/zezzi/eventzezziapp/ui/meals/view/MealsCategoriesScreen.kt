0e9984a9fe974289392f660b9a1e0bf0275a6230
Nelson <gar22434@uvg.edu.gt>
2023-10-09 07:46:29 -0600

    lab 9 terminado, ya se pudo ver la aplicación, arreglaron el sitio

--- EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/meals/view/MealsCategoriesScreen.kt
+++ EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/meals/view/MealsCategoriesScreen.kt
@@ -1,12 +1,15 @@
 package com.zezzi.eventzezziapp.ui.meals.view
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -14,7 +17,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -22,8 +27,8 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
-import com.zezzi.eventzezziapp.navigation.AppBar
 import com.zezzi.eventzezziapp.data.networking.response.MealResponse
+import com.zezzi.eventzezziapp.navigation.AppBar
 import kotlinx.coroutines.runBlocking
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -48,56 +53,72 @@ fun MealsCategoriesScreen(
         LazyColumn(contentPadding = it) {
             items(rememberedMeals.value) { meal ->
                 Box(
-                    modifier = Modifier.padding(2.dp)
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .fillMaxSize()
+                        .clip(RoundedCornerShape(10))
+                        .background(Color.LightGray)
                 ) {
-                    Row(){
-                        AsyncImage(
-                            model = meal.imageUrl,
-                            contentDescription = meal.description
-                        )
-                        Column(
-                            modifier = Modifier.padding(2.dp)
-                        ) {
-                            Text(
-                                text = "Category: ",
-                                modifier = Modifier
-                                    .padding(2.dp),
-                                color = Color.Black,
-                                fontSize = 20.sp,
-                                fontWeight = FontWeight.Bold
+                    Column(
+                        modifier = Modifier.padding(2.dp),
+                        verticalArrangement = Arrangement.SpaceBetween,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Row(){
+                            AsyncImage(
+                                model = meal.imageUrl,
+                                contentDescription = meal.description
                             )
-                            Text(
-                                text = meal.name,
-                                modifier = Modifier
-                                    .padding(2.dp),
-                                color = Color.Black,
-                                fontSize = 20.sp,
-                                fontWeight = FontWeight.Bold
-                            )
-                            Text(
-                                text = "Description: ",
-                                modifier = Modifier
-                                    .padding(2.dp),
-                                color = Color.Black,
-                                fontSize = 20.sp,
-                                fontWeight = FontWeight.Bold
-                            )
-                            Text(
-                                text = meal.description,
-                                modifier = Modifier
-                                    .padding(2.dp),
-                                color = Color.Black,
-                                fontSize = 20.sp,
-                                fontWeight = FontWeight.Bold
-                            )
-                            Text(
-                                text = "Date: Date 10 Month 8 Day ",
-                                modifier = Modifier
-                                    .padding(2.dp),
-                                color = Color.Black,
-                                fontSize = 20.sp,
-                                fontWeight = FontWeight.Bold
-                            )
+                            Column(
+                                modifier = Modifier.padding(2.dp)
+                            ) {
+                                Text(
+                                    text = "Category: ",
+                                    modifier = Modifier
+                                        .padding(2.dp),
+                                    color = Color.Black,
+                                    fontSize = 20.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                                Text(
+                                    text = meal.name,
+                                    modifier = Modifier
+                                        .padding(2.dp),
+                                    color = Color.Black,
+                                    fontSize = 20.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+
+                                Text(
+                                    text = "Date: 10 Month 8 Day ",
+                                    modifier = Modifier
+                                        .padding(2.dp),
+                                    color = Color.Black,
+                                    fontSize = 20.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            }
+                        }
+                        Row() {
+                            Column(
+                                modifier = Modifier.padding(2.dp)
+                            ) {
+                                Text(
+                                    text = "Description: ",
+                                    modifier = Modifier
+                                        .padding(2.dp),
+                                    color = Color.Black,
+                                    fontSize = 20.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                                Text(
+                                    text = meal.description+"\n",
+                                    modifier = Modifier
+                                        .padding(2.dp),
+                                    color = Color.Black,
+                                    fontSize = 15.sp
+                                )
+                            }
                         }
                     }
                 }

ca0447f0d31bca1b938f53ef5b60bd1460c2217d
Nelson <gar22434@uvg.edu.gt>
2023-10-21 13:21:15 -0600

    commit

--- EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/data/networking/response/MealResponse.kt
+++ EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/data/networking/response/MealResponse.kt
@@ -2,11 +2,10 @@ package com.zezzi.eventzezziapp.data.networking.response
 
 import com.google.gson.annotations.SerializedName
 
-data class MealsCategoriesResponse(val categories: List<MealResponse>)
+data class MealCatResponse(val meals: List<MealResponse>)
 
 data class MealResponse(
-    @SerializedName("idCategory") val id: String,
-    @SerializedName("strCategory") val name: String,
-    @SerializedName("strCategoryDescription") val description: String,
-    @SerializedName("strCategoryThumb") val imageUrl: String
+    @SerializedName("strMeal") val name: String,
+    @SerializedName("strMealThumb") val imageUrl: String,
+    @SerializedName("idMeal") val id: String
 )
